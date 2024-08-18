450d421df2845ac8da8dd71b95777b59d4be03c2
kazuma <tom071003@gmail.com>
2024-08-18 21:29:12 +0200

    .

--- model/sphere.js
+++ model/sphere.js
@@ -8,18 +8,123 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.SphereGeometry(1, 15, 15);
-const material = new THREE.MeshBasicMaterial( { color: 0xff477e } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+//--------------------shadow---------------------------------------
+//Create a WebGLRenderer and turn on shadows in the renderer
+const rendere = new THREE.WebGLRenderer();
+rendere.shadowMap.enabled = true;
+rendere.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
 
-camera.position.z = 5;
+//Create a DirectionalLight and turn on shadows for the light
+const light = new THREE.DirectionalLight( 0xffffff, 3 );
+light.position.set( 6, 12, 3 ); //default; light shining from top
+light.castShadow = true; // default false
+scene.add( light );
+
+//Set up shadow properties for the light
+light.shadow.mapSize.width = 512; // default
+light.shadow.mapSize.height = 512; // default
+light.shadow.camera.near = 0.5; // default
+light.shadow.camera.far = 500; // default
+
+//Create a sphere that cast shadows (but does not receive them)
+const vertexShader = `
+    varying vec2 vUv;
+    void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+`;
+
+const fragmentShader = `
+    varying vec2 vUv;
+    void main() {
+        vec3 color1 = vec3(0.4, 0, 0.8 );
+        vec3 color2 = vec3(0.5, 1, 1);
+        vec3 color = mix(color1, color2, vUv.y);
+        gl_FragColor = vec4(color, 1.0);
+    }
+`;
+
+const material = new THREE.ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader
+});
+
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+const sphere = new THREE.Mesh(sphereGeometry, material);
+sphere.castShadow = true; // default is false
+sphere.receiveShadow = false; // default
+scene.add(sphere);
+
+// //--------------------line right---------------------------------------
+// const LRightGeometry = new THREE.BoxGeometry(5, 5, 0.01);
+// const LRightMaterial = new THREE.MeshLambertMaterial( { color: 0xfff } );
+// const LRight = new THREE.Mesh( LRightGeometry, LRightMaterial );
+// // LRight.translateX( 0 );
+// // LRight.translateY( 0 );
+// LRight.castShadow = true; //default is false
+// LRight.receiveShadow = false; //default
+// scene.add( LRight );
+
+// //--------------------line left---------------------------------------
+// const LLeftGeometry = new THREE.BoxGeometry(0.2, 5, 0);
+// const LLeftMaterial = new THREE.MeshLambertMaterial( { color: 0xfff } );
+// const LLeft = new THREE.Mesh( LLeftGeometry, LLeftMaterial );
+// LLeft.translateX( - 14.8 );
+// LLeft.translateY( 0 );
+// LLeft.castShadow = true; //default is false
+// LLeft.receiveShadow = false; //default
+// scene.add( LLeft );
+
+//Create a plane that receives shadows (but does not cast them)
+const planeGeometry = new THREE.PlaneGeometry( 30, 20, 1, 1 );
+const planeMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff } )
+const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+plane.receiveShadow = true;
+scene.add( plane );
+
+//--------------------cadre---------------------------------------
+const mat = new THREE.LineBasicMaterial( { color: 0xffffff } );
+
+const points = [];
+points.push( new THREE.Vector3( - 15, 10, 0 ) );
+points.push( new THREE.Vector3( 15, 10, 0 ) );
+points.push( new THREE.Vector3( 15, - 10, 0 ) );
+points.push( new THREE.Vector3( - 15, -10, 0 ) );
+points.push( new THREE.Vector3( - 15, 10, 0 ) );
+const geo = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geo, mat );
+scene.add( line );
+
+//--------------------line right---------------------------------------
+const LRightGeometry = new THREE.BoxGeometry(0.2, 5, 0);
+const LRightMaterial = new THREE.MeshBasicMaterial( { color: 0xfff } );
+const LRight = new THREE.Mesh( LRightGeometry, LRightMaterial );
+LRight.translateX( 14.8 );
+LRight.translateY( 0 );
+LRight.castShadow = true; //default is false
+LRight.receiveShadow = false; //default
+scene.add( LRight );
+
+//--------------------line left---------------------------------------
+const LLeftGeometry = new THREE.BoxGeometry(0.2, 5, 0);
+const LLeftMaterial = new THREE.MeshBasicMaterial( { color: 0xfff } );
+const LLeft = new THREE.Mesh( LLeftGeometry, LLeftMaterial );
+LLeft.translateX( - 14.8 );
+LLeft.translateY( 0 );
+LLeft.castShadow = true; //default is false
+LLeft.receiveShadow = false; //default
+scene.add( LLeft );
+
+//--------------------affichage---------------------------------------
+
+camera.position.z = 15;
 
 function animate() {
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
+	sphere.rotation.x += 0.01;
+	sphere.rotation.y += 0.01;
+queueMicrotask
 	renderer.render( scene, camera );
 
 }
--- threeJS/main.js
+++ threeJS/main.js
@@ -8,103 +8,93 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-//--------------------shadow---------------------------------------
-//Create a WebGLRenderer and turn on shadows in the renderer
-const rendere = new THREE.WebGLRenderer();
-rendere.shadowMap.enabled = true;
-rendere.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-
-//Create a DirectionalLight and turn on shadows for the light
-const light = new THREE.DirectionalLight( 0xffffff, 3 );
-light.position.set( 6, 12, 10 ); //default; light shining from top
-light.castShadow = true; // default false
-scene.add( light );
-
-//Set up shadow properties for the light
-light.shadow.mapSize.width = 512; // default
-light.shadow.mapSize.height = 512; // default
-light.shadow.camera.near = 0.5; // default
-light.shadow.camera.far = 500; // default
-
-//Create a sphere that cast shadows (but does not receive them)
-const sphereGeometry = new THREE.SphereGeometry( 0.5, 32, 32 );
-const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xfff } );
+//------------------sphere-----------------------------------------
+const sphereGeometry = new THREE.SphereGeometry( 0.8, 32, 32 );
+const sphereMaterial = new THREE.MeshBasicMaterial( { color: 0xf99ff } );
 const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-sphere.castShadow = true; //default is false
-sphere.receiveShadow = false; //default
+sphere.translateX( 0 );
+sphere.translateY( 0 );
+sphere.translateZ( 0.8 );
 scene.add( sphere );
 
-// //--------------------line right---------------------------------------
-// const geom = new THREE.BoxGeometry(0.2, 5, 0);
-// const mate = new THREE.MeshLambertMaterial( { color: 0xfff } );
-// const cu = new THREE.Mesh( geom, mate );
-// cu.translateX( 14.8 );
-// cu.translateY( 0 );
-// cu.castShadow = true; //default is false
-// cu.receiveShadow = false; //default
-// scene.add( cu );
-
-// //--------------------line left---------------------------------------
-// const geome = new THREE.BoxGeometry(0.2, 5, 0);
-// const mater = new THREE.MeshLambertMaterial( { color: 0xfff } );
-// const cub = new THREE.Mesh( geome, mater );
-// cub.translateX( - 14.8 );
-// cub.translateY( 0 );
-// cub.castShadow = true; //default is false
-// cub.receiveShadow = false; //default
-// scene.add( cub );
-
-//Create a plane that receives shadows (but does not cast them)
-const planeGeometry = new THREE.PlaneGeometry( 30, 20, 32, 32 );
-const planeMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff } )
-const plane = new THREE.Mesh( planeGeometry, planeMaterial );
-plane.receiveShadow = true;
-scene.add( plane );
-
-//--------------------cadre---------------------------------------
-const mat = new THREE.LineBasicMaterial( { color: 0xffffff } );
-
-const points = [];
-points.push( new THREE.Vector3( - 15, 10, 0 ) );
-points.push( new THREE.Vector3( 15, 10, 0 ) );
-points.push( new THREE.Vector3( 15, - 10, 0 ) );
-points.push( new THREE.Vector3( - 15, -10, 0 ) );
-points.push( new THREE.Vector3( - 15, 10, 0 ) );
-const geo = new THREE.BufferGeometry().setFromPoints( points );
-const line = new THREE.Line( geo, mat );
-scene.add( line );
+//--------------------arene------------------------------------------
+const AreneGeomerty = new THREE.BoxGeometry(30, 20, 1);
+const AreneMaterial = new THREE.MeshBasicMaterial( { color: 0xB72FED } );
+const Arene = new THREE.Mesh( AreneGeomerty, AreneMaterial );
+Arene.translateZ( -0.5 );
+scene.add( Arene );
 
 //--------------------line right---------------------------------------
-const geom = new THREE.BoxGeometry(0.2, 5, 0);
-const mate = new THREE.MeshBasicMaterial( { color: 0xfff } );
-const cu = new THREE.Mesh( geom, mate );
-cu.translateX( 14.8 );
-cu.translateY( 0 );
-cu.castShadow = true; //default is false
-cu.receiveShadow = false; //default
-scene.add( cu );
+const LRightGeometry = new THREE.BoxGeometry(0.5, 5, 1);
+const LRightMaterial = new THREE.MeshBasicMaterial( { color: 0xfff } );
+const LRight = new THREE.Mesh( LRightGeometry, LRightMaterial );
+LRight.translateX( 14.5 );
+LRight.translateZ( 0.5 );
+LRight.translateY( 0 );
+scene.add( LRight );
 
 //--------------------line left---------------------------------------
-const geome = new THREE.BoxGeometry(0.2, 5, 0);
-const mater = new THREE.MeshBasicMaterial( { color: 0xfff } );
-const cub = new THREE.Mesh( geome, mater );
-cub.translateX( - 14.8 );
-cub.translateY( 0 );
-cub.castShadow = true; //default is false
-cub.receiveShadow = false; //default
-scene.add( cub );
+const LLeftGeometry = new THREE.BoxGeometry(0.5, 5, 1);
+const LLeftMaterial = new THREE.MeshBasicMaterial( { color: 0xfff } );
+const LLeft = new THREE.Mesh( LLeftGeometry, LLeftMaterial );
+LLeft.translateX( - 14.5 );
+LLeft.translateZ( 0.5 );
+LLeft.translateY( 0 );
+scene.add( LLeft );
+
+//-------------------bordure------------------------------------------
+const LborderGeometry = new THREE.BoxGeometry(0.2, 19.6, 0.2);
+const LbordertMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const Lborder = new THREE.Mesh( LborderGeometry, LbordertMaterial );
+Lborder.translateX( - 14.9 );
+Lborder.translateZ( 0.1 );
+Lborder.translateY( 0 );
+scene.add( Lborder );
+
+const RborderGeometry = new THREE.BoxGeometry(0.2, 19.6, 0.2);
+const RbordertMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const Rborder = new THREE.Mesh( RborderGeometry, RbordertMaterial );
+Rborder.translateX( 14.9 );
+Rborder.translateZ( 0.1 );
+Rborder.translateY( 0 );
+scene.add( Rborder );
+
+const SborderGeometry = new THREE.BoxGeometry(30, 0.2, 0.2);
+const SbordertMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const Sborder = new THREE.Mesh( SborderGeometry, SbordertMaterial );
+Sborder.translateX( 0 );
+Sborder.translateZ( 0.1 );
+Sborder.translateY( -9.9 );
+scene.add( Sborder );
+
+const NborderGeometry = new THREE.BoxGeometry(30, 0.2, 0.2);
+const NbordertMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+const Nborder = new THREE.Mesh( NborderGeometry, NbordertMaterial );
+Nborder.translateX( 0 );
+Nborder.translateZ( 0.1 );
+Nborder.translateY( 9.9 );
+scene.add( Nborder );
+
+//-------------------axe------------------------------------------
+const material = new THREE.LineBasicMaterial( { color: 0xffffff } );
+const points = [];
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 0, -10, 0 ) );
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+const line = new THREE.Line( geometry, material );
+scene.add( line );
 
 //--------------------affichage---------------------------------------
 
-//Create a helper for the shadow camera (optional)
-const helper = new THREE.CameraHelper( light.shadow.camera );
-
 camera.position.z = 15;
+camera.position.y = -20;
+camera.position.x = 0;
+camera.rotateX(45);
 
 function animate() {
 
-	sphere.rotation.x += 0.01;
-	sphere.rotation.y += 0.01;
+	// sphere.rotation.x += 0.01;
+	// sphere.rotation.y += 0.01;
 queueMicrotask
 	renderer.render( scene, camera );
 
