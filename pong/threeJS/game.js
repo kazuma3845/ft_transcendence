5fd4032aff649d77e88edf4edfea86a4de197305
kazuma <tom071003@gmail.com>
2024-08-27 16:49:56 +0200

    game ok

--- pong/threeJS/game.js
+++ pong/threeJS/game.js
@@ -3,34 +3,36 @@ import Form from './form.js';
 import Pong from './mouvement.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
 
 const form = new Form();
-const pong = new Pong(form); // Passer l'instance de form à Pong
+const pong = new Pong(form);
 
-scene.add(form.ball);
-scene.add(form.Arene);
-scene.add(form.paddleRight);
-scene.add(form.paddleLeft);
-scene.add(form.Lborder);
-scene.add(form.Rborder);
-scene.add(form.Sborder);
-scene.add(form.Nborder);
-scene.add(form.line);
+scene.add( form.ball );
+scene.add( form.Arene );
+scene.add( form.paddleRight );
+scene.add( form.paddleLeft );
+scene.add( form.Lborder );
+scene.add( form.Rborder );
+scene.add( form.Sborder );
+scene.add( form.Nborder );
+scene.add( form.line );
+
+//--------------------affichage---------------------------------------
 
 camera.position.z = 150;
 camera.position.y = -200;
 camera.rotateX(45);
 
-function animate(timestamp) {
-    pong.deplacerRaquette(timestamp); // Met à jour les positions des raquettes
-    // pong.updateBallPosition(); // Décommentez pour animer la balle
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate); // Boucle d'animation
-}
 
-requestAnimationFrame(animate); // Démarrer l'animation
+function animate() {
+	queueMicrotask
+    requestAnimationFrame(animate);
+	pong.updateBallPosition();
+	renderer.render( scene, camera );
+}
--- pong/threeJS/mouvement.js
+++ pong/threeJS/mouvement.js
@@ -1,13 +1,12 @@
-import Form from './form.js';
 export default class Pong {
-    constructor() {
-        this.form = new Form();
+    constructor(form) {
+        this.form = form;
         this.arenaWidth = this.form.arene_size[0] - (2 * this.form.LRborder_size[0]);
         this.arenaHeight = this.form.arene_size[1] - (2 * this.form.NSborder_size[1]);
         this.initialSpeed = 0.005;
         this.ballSpeedX = this.initialSpeed;
         this.ballSpeedY = 0;
-        this.ballPaused = false;
+        this.ballPaused = true;
         this.keysPressed = {};
         this.paddle_move_speed = 4;
 
@@ -15,7 +14,7 @@ export default class Pong {
         window.addEventListener('keyup', this.handleKeyUp.bind(this));
         window.addEventListener('keydown', this.handleKeyPress.bind(this));
 
-        // this.animate();
+        this.animate();
     }
 
     handleKeyDown(event) {
@@ -79,75 +78,71 @@ export default class Pong {
         }
     }
 
-    // updateBallPosition() {
-    //     if (this.ballPaused) {
-    //         if (this.form.ball.position.x < 0) {
-    //             this.form.ball.position.y = this.form.paddleLeft.position.y;
-    //         }
-    //         if (this.form.ball.position.x > 0) {
-    //             this.form.ball.position.y = this.form.paddleRight.position.y;
-    //         }
-    //         return;
-    //     }
-    //     // this.form.ball.rotation.x += this.ballSpeedY * 0.1;
-    //     // this.form.ball.rotation.y += this.ballSpeedX * 0.1;
+    updateBallPosition() {
+        if (this.ballPaused) {
+            if (this.form.ball.position.x < 0) {
+                this.form.ball.position.y = this.form.paddleLeft.position.y;
+            }
+            if (this.form.ball.position.x > 0) {
+                this.form.ball.position.y = this.form.paddleRight.position.y;
+            }
+            return;
+        }
+        this.form.ball.rotation.x += this.ballSpeedY * 0.1;
+        this.form.ball.rotation.y += this.ballSpeedX * 0.1;
 
-    //     this.form.ball.position.x += this.ballSpeedX;
-    //     this.form.ball.position.y += this.ballSpeedY;
+        this.form.ball.position.x += this.ballSpeedX;
+        this.form.ball.position.y += this.ballSpeedY;
 
-    //     const halfArenaWidth = this.arenaWidth / 2;
-    //     const halfArenaHeight = this.arenaHeight / 2;
+        const halfArenaWidth = this.arenaWidth / 2;
+        const halfArenaHeight = this.arenaHeight / 2;
 
-    //     if ((this.form.ball.position.x + form.ballRayon) >= halfArenaWidth) {
-    //         this.form.ball.position.x = this.form.paddleRight.position.x - form.paddle_size[0] / 2 - form.ballRayon;
-    //         this.form.ball.position.y = this.form.paddleRight.position.y;
-    //         this.ballSpeedX = -Math.abs(this.ballSpeedX);
-    //         this.ballPaused = true;
-    //     }
+        if ((this.form.ball.position.x + this.form.ballRayon) >= halfArenaWidth) {
+            this.form.ball.position.x = this.form.paddleRight.position.x - this.form.paddle_size[0] / 2 - this.form.ballRayon;
+            this.form.ball.position.y = this.form.paddleRight.position.y;
+            this.ballSpeedX = -Math.abs(this.ballSpeedX);
+            this.ballPaused = true;
+        }
 
-    //     if ((this.form.ball.position.x - form.ballRayon) <= -halfArenaWidth) {
-    //         this.form.ball.position.x = this.form.paddleLeft.position.x + form.paddle_size[0] / 2 + form.ballRayon;
-    //         this.form.ball.position.y = this.form.paddleLeft.position.y;
-    //         this.ballSpeedX = Math.abs(this.ballSpeedX);
-    //         this.ballPaused = true;
-    //     }
+        if ((this.form.ball.position.x - this.form.ballRayon) <= -halfArenaWidth) {
+            this.form.ball.position.x = this.form.paddleLeft.position.x + this.form.paddle_size[0] / 2 + this.form.ballRayon;
+            this.form.ball.position.y = this.form.paddleLeft.position.y;
+            this.ballSpeedX = Math.abs(this.ballSpeedX);
+            this.ballPaused = true;
+        }
         
-    //     if ((this.form.ball.position.y + form.ballRayon) >= halfArenaHeight || (this.form.ball.position.y - form.ballRayon) <= -halfArenaHeight) {
-    //         this.ballSpeedY = -this.ballSpeedY;
-    //     }
+        if ((this.form.ball.position.y + this.form.ballRayon) >= halfArenaHeight || (this.form.ball.position.y - this.form.ballRayon) <= -halfArenaHeight) {
+            this.ballSpeedY = -this.ballSpeedY;
+        }
         
-    //     //-----------------paddle----------------------------
-        // const halfRaquetteHeight = form.paddle_size[1] / 2;
-        // const halfRaquetteWidth = form.paddle_size[0] / 2;
+        //-----------------paddle----------------------------
+        const halfRaquetteHeight = this.form.paddle_size[1] / 2;
+        const halfRaquetteWidth = this.form.paddle_size[0] / 2;
                 
-        // if (this.form.ball.position.x - form.ballRayon <= this.form.paddleLeft.position.x + halfRaquetteWidth &&
-        //     this.form.ball.position.y >= this.form.paddleLeft.position.y - halfRaquetteHeight &&
-        //     this.form.ball.position.y <= this.form.paddleLeft.position.y + halfRaquetteHeight) {
-        //     const impactY = this.form.ball.position.y - this.form.paddleLeft.position.y;
-        //     const normalizedImpactY = impactY / halfRaquetteHeight;
-        //     const bounceAngle = normalizedImpactY * (Math.PI / 4);
-        //     this.ballSpeedX = Math.abs(this.ballSpeedX) * Math.cos(bounceAngle);
-        //     this.ballSpeedY = Math.abs(this.ballSpeedX) * Math.sin(bounceAngle);
+        if (this.form.ball.position.x - this.form.ballRayon <= this.form.paddleLeft.position.x + halfRaquetteWidth &&
+            this.form.ball.position.y >= this.form.paddleLeft.position.y - halfRaquetteHeight &&
+            this.form.ball.position.y <= this.form.paddleLeft.position.y + halfRaquetteHeight) {
+            const impactY = this.form.ball.position.y - this.form.paddleLeft.position.y;
+            const normalizedImpactY = impactY / halfRaquetteHeight;
+            const bounceAngle = normalizedImpactY * (Math.PI / 4);
+            this.ballSpeedX = Math.abs(this.ballSpeedX) * Math.cos(bounceAngle);
+            this.ballSpeedY = Math.abs(this.ballSpeedX) * Math.sin(bounceAngle);
+
+        }
         
-        //     const speed = Math.sqrt(this.ballSpeedX * this.ballSpeedX + this.ballSpeedY * this.ballSpeedY);
-        //     this.ballSpeedX = (this.ballSpeedX / speed) * this.initialSpeed;
-        //     this.ballSpeedY = (this.ballSpeedY / speed) * this.initialSpeed;
-        // }
-        
-        // if (this.form.ball.position.x + form.ballRayon >= this.form.paddleRight.position.x - halfRaquetteWidth &&
-        //     this.form.ball.position.y >= this.form.paddleRight.position.y - halfRaquetteHeight &&
-        //     this.form.ball.position.y <= this.form.paddleRight.position.y + halfRaquetteHeight) {
-        //     const impactY = this.form.ball.position.y - this.form.paddleRight.position.y;
-        //     const normalizedImpactY = impactY / halfRaquetteHeight;
-        //     const bounceAngle = normalizedImpactY * (Math.PI / 4);
-        //     this.ballSpeedX = -Math.abs(this.ballSpeedX) * Math.cos(bounceAngle);
-        //     this.ballSpeedY = Math.abs(this.ballSpeedX) * Math.sin(bounceAngle);
-        
-        //     const speed = Math.sqrt(this.ballSpeedX * this.ballSpeedX + this.ballSpeedY * this.ballSpeedY);
-        //     this.ballSpeedX = (this.ballSpeedX / speed) * this.initialSpeed;
-        //     this.ballSpeedY = (this.ballSpeedY / speed) * this.initialSpeed;
-        // }
-    // }
+        if (this.form.ball.position.x + this.form.ballRayon >= this.form.paddleRight.position.x - halfRaquetteWidth &&
+            this.form.ball.position.y >= this.form.paddleRight.position.y - halfRaquetteHeight &&
+            this.form.ball.position.y <= this.form.paddleRight.position.y + halfRaquetteHeight) {
+            const impactY = this.form.ball.position.y - this.form.paddleRight.position.y;
+            const normalizedImpactY = impactY / halfRaquetteHeight;
+            const bounceAngle = normalizedImpactY * (Math.PI / 4);
+            this.ballSpeedX = -Math.abs(this.ballSpeedX) * Math.cos(bounceAngle);
+            this.ballSpeedY = Math.abs(this.ballSpeedX) * Math.sin(bounceAngle);
+        }
+        const speed = Math.sqrt(this.ballSpeedX * this.ballSpeedX + this.ballSpeedY * this.ballSpeedY);
+        this.ballSpeedX = (this.ballSpeedX / speed) * this.initialSpeed;
+        this.ballSpeedY = (this.ballSpeedY / speed) * this.initialSpeed;
+    }
 
     animate(timestamp) {
         if (!this.lastTime) {
@@ -158,9 +153,6 @@ export default class Pong {
         this.lastTime = timestamp;
     
         this.deplacerRaquette(deltaTime);
-        // this.updateBallPosition();
         requestAnimationFrame(this.animate.bind(this));
     }
-    
-    
 }
