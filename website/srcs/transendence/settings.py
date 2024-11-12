2f8c89aa01830ec04572b5a34a2c7401c4bb6b85
frobain <frobain@student.42.ch>
2024-11-12 15:59:09 +0100

    added custom password validator

--- website/srcs/transendence/settings.py
+++ website/srcs/transendence/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY_DJANGO")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['transcendence', 'localhost', config('IP_LOCAL')]
+ALLOWED_HOSTS = ["transcendence", "localhost", config("IP_LOCAL")]
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -39,6 +39,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+    {"NAME": "users.validators.CustomPasswordValidator"},
 ]
 
 # Application definition
@@ -126,25 +127,6 @@ DATABASES = {
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
 
 
 # Internationalization
