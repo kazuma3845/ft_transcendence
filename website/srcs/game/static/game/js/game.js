42fc03338d3d409ab891c214fa9a0a1ab58b2057
frobain <frobain@student.42.ch>
2024-11-14 14:55:23 +0100

    game modal fixed

--- website/srcs/game/static/game/js/game.js
+++ website/srcs/game/static/game/js/game.js
@@ -1,4 +1,3 @@
-
 const allowedValues = [1, 3, 5, 7, 9, 11];
 function loadGameForm() {
   fetch("/static/game/html/game-form.html")
@@ -9,19 +8,23 @@ function loadGameForm() {
     });
 }
 
-async function createGameSession(data, player1 = null, player2 = null, tourDataId = null) {
+async function createGameSession(
+  data,
+  player1 = null,
+  player2 = null,
+  tourDataId = null
+) {
   // Ajoute player1 et player2 s'ils sont définis
   if (player1) data["player1"] = player1;
   if (player2) data["player2"] = player2;
   data["tour"] = tourDataId;
-
 
   // Envoie les données à l'API pour créer une nouvelle session de jeu
   const response = await fetch("/api/game/sessions/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "X-CSRFToken": getCSRFToken(), // Assure-toi que cette fonction est bien définie
+      "X-CSRFToken": getCSRFToken(),
     },
     body: JSON.stringify(data),
   });
@@ -32,7 +35,11 @@ async function createGameSession(data, player1 = null, player2 = null, tourDataI
   return data_1;
 }
 
-function attachGameFormSubmitListener(player1 = null, player2 = null, invited = null) {
+function attachGameFormSubmitListener(
+  player1 = null,
+  player2 = null,
+  invited = null
+) {
   document
     .getElementById("game-form")
     .addEventListener("submit", function (event) {
@@ -64,13 +71,15 @@ function attachGameFormSubmitListener(player1 = null, player2 = null, invited = 
         .then((sessionData) => {
           let sessionId = sessionData.id;
           window.location.href = `/#game?sessionid=${sessionId}`;
-          if (invited)
-            sendInvitation(activeConversationId, sessionId);
+          if (invited) sendInvitation(activeConversationId, sessionId);
         })
         .catch((error) => {
-          console.error("Erreur lors de la création de la session de jeu:", error);
-          document.getElementById('error-message').textContent = error.message;
-          document.getElementById('error-message').style.display = 'block';
+          console.error(
+            "Erreur lors de la création de la session de jeu:",
+            error
+          );
+          document.getElementById("error-message").textContent = error.message;
+          document.getElementById("error-message").style.display = "block";
         });
     });
 }
@@ -167,7 +176,6 @@ function updateCheckboxValue(spanId, isChecked) {
   }
 }
 
-
 function showBotDiff(isChecked) {
   const botDiv = document.getElementById("bot-difficulty-div");
   if (isChecked) {
@@ -176,7 +184,9 @@ function showBotDiff(isChecked) {
     botDiv.style.display = "none";
   }
 }
+
 let multi = false;
+
 function handleMultiplayerChange(isChecked) {
   const botOptions = document.getElementById("bot-options");
   const botCheckbox = document.getElementById("bot");
@@ -185,7 +195,8 @@ function handleMultiplayerChange(isChecked) {
     multi = true;
     botOptions.style.display = "none";
     botCheckbox.checked = false;
-    botCheckbox.dispatchEvent(new Event('change')); //ou appeler updateCheckboxValue()
+    botCheckbox.dispatchEvent(new Event("change", { bubbles: true }));
+    //ou appeler updateCheckboxValue()
   } else {
     multi = false;
     botOptions.style.display = "block";
@@ -212,12 +223,12 @@ function updateWinNumber() {
 
 // Set initial value on load
 window.onload = function () {
-    // Vérifier si l'élément avec l'ID "game-form" est présent dans le DOM
-    const gameForm = document.getElementById("game-form");
+  // Vérifier si l'élément avec l'ID "game-form" est présent dans le DOM
+  const gameForm = document.getElementById("game-form");
 
   if (gameForm) {
-      // Si l'élément existe, appelez la fonction updateWinNumber
-      updateWinNumber();
+    // Si l'élément existe, appelez la fonction updateWinNumber
+    updateWinNumber();
   }
 };
 
@@ -284,11 +295,11 @@ async function joinGame(sessionId) {
       );
     }
     // Si l'appel API est réussi, lancer la fonction pour charger le jeu
-        console.log(`Vous avez rejoint la session ${sessionId}`);
-        window.location.href = `/#game?sessionid=${sessionId}`;
-    } catch (error) {
-        console.error("Erreur lors de la connexion à la session:", error);
-    }
+    console.log(`Vous avez rejoint la session ${sessionId}`);
+    window.location.href = `/#game?sessionid=${sessionId}`;
+  } catch (error) {
+    console.error("Erreur lors de la connexion à la session:", error);
+  }
 }
 
 let win_number;
@@ -318,39 +329,49 @@ function startWebSocket(sessionId) {
   this.pingInterval = null;
   const socket = new WebSocket(`/ws/game/sessions/${sessionId}/`);
 
-let point1 = 0
-let point2 = 0
-let player2 = null
+  let point1 = 0;
+  let point2 = 0;
+  let player2 = null;
 
-socket.onopen = async function (e) {
-    const gameSession = await getSession(sessionId)
-    console.log("GAMESESSION: ", gameSession)
-    win_number = gameSession.win_number
-    point1 = gameSession.player1_points
-    point2 = gameSession.player2_points
+  socket.onopen = async function (e) {
+    const gameSession = await getSession(sessionId);
+    console.log("GAMESESSION: ", gameSession);
+    win_number = gameSession.win_number;
+    point1 = gameSession.player1_points;
+    point2 = gameSession.player2_points;
     console.log("WebSocket connected.");
   };
 
   socket.onmessage = async function (e) {
     // console.log('Message received:', e.data);
     try {
-    const data = JSON.parse(e.data);
-    // console.log("F-E: client websocket parsed data:", data);
-    if (data.type === "game_score") {
-      point1 = data.player1_points;
-      point2 = data.player2_points;
-      updateScoreDisplay(data.player1, player2, data.player1_points, data.player2_points);
-    }
-    if (data.type === "display_player") {
-      player2 = data.player2;
-      player1 = data.player1;
-      displayPlayer(data.player1, data.player2);
-    }
-    if (data.type === "disconnect_screen") {
-      if (win_number !== point1 && win_number !== point2 && player2 !== "Bot" && player2 !== "LocalPlayer")
-        displayForfaitMessage(data.content == player1 ? player2 : player1);
+      const data = JSON.parse(e.data);
+      // console.log("F-E: client websocket parsed data:", data);
+      if (data.type === "game_score") {
+        point1 = data.player1_points;
+        point2 = data.player2_points;
+        updateScoreDisplay(
+          data.player1,
+          player2,
+          data.player1_points,
+          data.player2_points
+        );
+      }
+      if (data.type === "display_player") {
+        player2 = data.player2;
+        player1 = data.player1;
+        displayPlayer(data.player1, data.player2);
+      }
+      if (data.type === "disconnect_screen") {
+        if (
+          win_number !== point1 &&
+          win_number !== point2 &&
+          player2 !== "Bot" &&
+          player2 !== "LocalPlayer"
+        )
+          displayForfaitMessage(data.content == player1 ? player2 : player1);
         setLobbyRedirect(sessionId);
-  }
+      }
     } catch (error) {
       console.error("Error parsing message:", error);
     }
@@ -362,23 +383,28 @@ socket.onopen = async function (e) {
 
   socket.onerror = function (e) {
     console.error("WebSocket error:", e);
-};
+  };
 
-  function displayForfaitMessage(Player_disconnect) {		
+  function displayForfaitMessage(Player_disconnect) {
     fetch(`/static/game/html/victory.html`)
-    .then((response) => response.text())
-    .then((html) => {
-      document.getElementById("app").innerHTML = html;
-      const winnerMessage = document.getElementById('winnerMessage');
-      winnerMessage.textContent = `${Player_disconnect} a left the game`;
-    });
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("app").innerHTML = html;
+        const winnerMessage = document.getElementById("winnerMessage");
+        winnerMessage.textContent = `${Player_disconnect} a left the game`;
+      });
   }
 
   function sleep(seconds) {
-    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+    return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
   }
 
-  async function checkWinCondition(username, username2, player1Points, player2Points) {
+  async function checkWinCondition(
+    username,
+    username2,
+    player1Points,
+    player2Points
+  ) {
     if (player1Points >= win_number) {
       await sleep(0.2);
       fetch(`/static/game/html/victory.html`)
@@ -400,7 +426,7 @@ socket.onopen = async function (e) {
 
   function displayWinnerMessage(winner) {
     // Sélectionner l'élément du message de victoire
-    const winnerMessage = document.getElementById('winnerMessage');
+    const winnerMessage = document.getElementById("winnerMessage");
     // Comparer les noms d'utilisateur et mettre à jour le message
     winnerMessage.textContent = `${winner} Win the game!`;
   }
@@ -409,22 +435,27 @@ socket.onopen = async function (e) {
     const gameSession = await getSession(sessionId);
     console.log(gameSession);
     const tourid = gameSession.tour;
-    const redirectLink = document.getElementById('redirect');
+    const redirectLink = document.getElementById("redirect");
 
     if (!redirectLink) {
-        return;
+      return;
     }
 
     if (tourid) {
-        redirectLink.href = `#tournaments?tourid=${tourid}`;
-        redirectLink.textContent = "Return to Lobby";
+      redirectLink.href = `#tournaments?tourid=${tourid}`;
+      redirectLink.textContent = "Return to Lobby";
     } else {
-        redirectLink.href = "#game";
-        redirectLink.textContent = "Play again";
+      redirectLink.href = "#game";
+      redirectLink.textContent = "Play again";
     }
-}
+  }
 
-  function updateScoreDisplay(username, username2, player1Points, player2Points) {
+  function updateScoreDisplay(
+    username,
+    username2,
+    player1Points,
+    player2Points
+  ) {
     const player1Elem = document.getElementById("player1");
     const player1ScoreElem = document.getElementById("player1Score");
     const player2ScoreElem = document.getElementById("player2Score");
@@ -460,12 +491,17 @@ async function getGame(game_session_id) {
     const response = await fetch(`/api/game/sessions/${game_session_id}/`);
 
     if (!response.ok) {
-      throw new Error(`Erreur lors de la récupération de la conversation: ${response.status}`);
+      throw new Error(
+        `Erreur lors de la récupération de la conversation: ${response.status}`
+      );
     }
 
     const session = await response.json();
-    return session;  // Retourne l'ID de la conversation
+    return session; // Retourne l'ID de la conversation
   } catch (error) {
-    console.error("Erreur lors de la récupération de la conversation du tournoi:", error);
+    console.error(
+      "Erreur lors de la récupération de la conversation du tournoi:",
+      error
+    );
   }
 }
